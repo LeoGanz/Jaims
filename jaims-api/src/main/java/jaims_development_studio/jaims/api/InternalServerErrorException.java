06d788246ca56cee219105184df562c2623da7ef
WilliGross <dergrossewilligross@gmail.com>
2018-02-03 15:14:40 +0100

    Fix profile persistence. Update on #85
    Fix persisting profiles received from the client. (Hibernate wants to
    set the UUID itself).
    Add InternalServerErrorException for occasions where the client just has
    to accept the server's failures.

---
+++ jaims-api/src/main/java/jaims_development_studio/jaims/api/InternalServerErrorException.java
@@ -0,0 +1,16 @@
+package jaims_development_studio.jaims.api;
+
+public class InternalServerErrorException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	public InternalServerErrorException() {
+		super();
+	}
+	
+	public InternalServerErrorException(String s) {
+		super(s);
+	}
+	
+}
--- jaims-server/src/main/java/jaims_development_studio/jaims/server/profile/ProfileManager.java
+++ jaims-server/src/main/java/jaims_development_studio/jaims/server/profile/ProfileManager.java
@@ -5,6 +5,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import javax.persistence.OptimisticLockException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jaims_development_studio.jaims.api.InternalServerErrorException;
 import jaims_development_studio.jaims.api.account.Account;
 import jaims_development_studio.jaims.api.profile.NoProfileAvailableException;
 import jaims_development_studio.jaims.api.profile.Profile;
@@ -19,64 +25,84 @@ import jaims_development_studio.jaims.api.sendables.SendableException;
 import jaims_development_studio.jaims.api.sendables.SendableProfile;
 import jaims_development_studio.jaims.api.sendables.SendableRequest;
 import jaims_development_studio.jaims.api.user.User;
+import jaims_development_studio.jaims.server.network.ClientManager;
 import jaims_development_studio.jaims.server.user.UserManager;
 
 public class ProfileManager {
-
+	
+	private final Logger				LOG	= LoggerFactory.getLogger(ClientManager.class);
 	private final UserManager			userManager;
 	private final ProfileDAO			profileDAO;
 	private final Map<UUID, Profile>	loadedProfiles;
-
+	
 	public ProfileManager(UserManager userManager) {
 		this.userManager = userManager;
 		profileDAO = new ProfileDAO();
 		loadedProfiles = new HashMap<>();
 	}
-
+	
 	public Profile newProfile(Account account, String nickname, String description, String status, byte[] profilePicture) throws ProfileAlreadyExistsException {
-
+		
 		if (get(account.getUuid()) != null)
 			throw new ProfileAlreadyExistsException(account.getUuid(), "A profile for the specified account / uuid already exists");
-
+		
 		Profile profile = new Profile(account, nickname, description, status, profilePicture, new Date());
 		loadedProfiles.put(account.getUuid(), profile);
-
+		
 		save(profile);
-
+		
 		return profile;
 	}
-
+	
 	public void saveOrUpdateProfile(SendableProfile sendableProfile) {
 		//Note: do not update profile field 'lastUpdated' as that would interfere with the clients update requests
 		Profile newProfile = sendableProfile.getProfile();
 		Profile oldProfile = get(newProfile.getUuid());
-
+		
 		if ((oldProfile != null) && (oldProfile.getLastUpdated().compareTo(newProfile.getLastUpdated()) > 0)) {
 			Exception exception = new ProfileUpdateFailedException("Profile Update Failed for profile with uuid " + newProfile.getUuid());
 			SendableException sendableException = new SendableException(exception);
 			sendSendable(sendableException, newProfile.getUuid());
 			return;
 		}
-
-		if (newProfile.getAccount() == null)
-			newProfile.setAccount(userManager.getAccountManager().get(newProfile.getUuid()));
-		save(newProfile);
-
 		
+		Account account = userManager.getAccountManager().get(newProfile.getUuid());
+		newProfile.setAccount(account);
+		
+		newProfile.setUuid(null);
+		oldProfile = null;
+		try {
+			save(newProfile);
+		} catch (OptimisticLockException e) {
+			LOG.error("Error when trying to persist new Profile: ", e);
+			InternalServerErrorException internalServerError = new InternalServerErrorException("Internal error when saving profiles!");
+			SendableException sendableException = new SendableException(internalServerError);
+			sendSendable(sendableException, account.getUuid());
+			return;
+		}
+		
+
 		SendableConfirmation sendableConfirmation = new SendableConfirmation(EConfirmationType.PROFILE_UPDATE_SUCCESFUL);
-		sendSendable(sendableConfirmation, newProfile.getUuid());
+		sendSendable(sendableConfirmation, account.getUuid());
+	}
+
+	public void save(Profile profile) {
+		if (profile != null) {
+			unload(profile);
+			profileDAO.saveOrUpdate(profile);
+		}
+	}
+	
+	private void unload(Profile profile) {
+		if (profile != null)
+			loadedProfiles.remove(profile.getUuid());
 	}
 	
 	private void sendSendable(Sendable sendable, UUID uuid) {
 		User user = userManager.get(uuid);
 		user.enqueueSendable(sendable);
 	}
-
-	public void save(Profile profile) {
-		if (profile != null)
-			profileDAO.saveOrUpdate(profile);
-	}
-
+	
 	public Profile get(UUID uuid) {
 		Profile result = loadedProfiles.get(uuid);
 		if (result == null) {
@@ -85,19 +111,19 @@ public class ProfileManager {
 		}
 		return result;
 	}
-
+	
 	public void manageProfileRequest(SendableRequest request) throws InvalidSendableTypeException, NoProfileAvailableException {
 		if (request.getRequestType() != ERequestType.PROFILE)
 			throw new InvalidSendableTypeException("Cannot process profile for a SendableRequest without RequestType PROFILE", request);
-
+		
 		Profile profile = get(request.getUniversalUuid());
 		if (profile == null)
 			throw new NoProfileAvailableException("There is no profile available for UUID " + request.getUniversalUuid());
-
+		
 		User user = userManager.get(request.getUniversalUuid());
-
+		
 		if ((request.getUniversalDate() == null) || (request.getUniversalDate().compareTo(profile.getLastUpdated()) < 0))
 			user.enqueueSendable(new SendableProfile(profile));
 	}
-
+	
 }
